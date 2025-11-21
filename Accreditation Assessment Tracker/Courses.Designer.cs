fc0f83e20c10381a10e298e24ddb16e986eda085
FLK44 <w10045584@usm.edu>
2025-11-21 04:30:19 -0600

    Adjusted DB to mirror design doc with relations and FK constraints.  Adjusted relevant functions to include the ID of referenced tables accordingly.

--- Accreditation Assessment Tracker/Courses.Designer.cs
+++ Accreditation Assessment Tracker/Courses.Designer.cs
@@ -44,15 +44,17 @@
             this.crsDescBox = new System.Windows.Forms.TextBox();
             this.submitBtn = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
             this.stdNumBox = new System.Windows.Forms.NumericUpDown();
             this.label11 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.crdHrsBox = new System.Windows.Forms.NumericUpDown();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.progIDDrpDwnBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.stdNumBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.crdHrsBox)).BeginInit();
@@ -73,7 +75,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(12, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(110, 20);
             this.label1.TabIndex = 1;
@@ -83,7 +85,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Location = new System.Drawing.Point(12, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 20);
             this.label2.TabIndex = 2;
@@ -93,7 +95,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Location = new System.Drawing.Point(12, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 20);
             this.label3.TabIndex = 3;
@@ -103,7 +105,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Location = new System.Drawing.Point(12, 132);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 20);
             this.label4.TabIndex = 4;
@@ -111,21 +113,21 @@
             // 
             // crsNameBox
             // 
-            this.crsNameBox.Location = new System.Drawing.Point(115, 19);
+            this.crsNameBox.Location = new System.Drawing.Point(134, 35);
             this.crsNameBox.Name = "crsNameBox";
             this.crsNameBox.Size = new System.Drawing.Size(196, 20);
             this.crsNameBox.TabIndex = 5;
             // 
             // crsCodeBox
             // 
-            this.crsCodeBox.Location = new System.Drawing.Point(115, 55);
+            this.crsCodeBox.Location = new System.Drawing.Point(134, 67);
             this.crsCodeBox.Name = "crsCodeBox";
             this.crsCodeBox.Size = new System.Drawing.Size(196, 20);
             this.crsCodeBox.TabIndex = 6;
             // 
             // preReqBox
             // 
-            this.preReqBox.Location = new System.Drawing.Point(91, 120);
+            this.preReqBox.Location = new System.Drawing.Point(134, 134);
             this.preReqBox.Name = "preReqBox";
             this.preReqBox.Size = new System.Drawing.Size(196, 20);
             this.preReqBox.TabIndex = 8;
@@ -133,7 +135,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 157);
+            this.label5.Location = new System.Drawing.Point(12, 171);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(658, 13);
             this.label5.TabIndex = 9;
@@ -143,7 +145,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 185);
+            this.label6.Location = new System.Drawing.Point(12, 199);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(127, 20);
             this.label6.TabIndex = 10;
@@ -153,7 +155,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(11, 230);
+            this.label8.Location = new System.Drawing.Point(11, 244);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(145, 20);
             this.label8.TabIndex = 12;
@@ -161,14 +163,14 @@
             // 
             // instNameBox
             // 
-            this.instNameBox.Location = new System.Drawing.Point(145, 187);
+            this.instNameBox.Location = new System.Drawing.Point(145, 201);
             this.instNameBox.Name = "instNameBox";
             this.instNameBox.Size = new System.Drawing.Size(196, 20);
             this.instNameBox.TabIndex = 13;
             // 
             // crsDescBox
             // 
-            this.crsDescBox.Location = new System.Drawing.Point(163, 232);
+            this.crsDescBox.Location = new System.Drawing.Point(163, 246);
             this.crsDescBox.Multiline = true;
             this.crsDescBox.Name = "crsDescBox";
             this.crsDescBox.Size = new System.Drawing.Size(224, 101);
@@ -200,6 +202,40 @@
             this.groupBox1.Size = new System.Drawing.Size(640, 182);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(270, 94);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(55, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(126, 94);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(143, 16);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Computer Engineering:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(125, 64);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(120, 16);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Computer Science:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(244, 62);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(55, 20);
+            this.textBox8.TabIndex = 15;
             // 
             // stdNumBox
             // 
@@ -240,50 +276,37 @@
             // 
             // crdHrsBox
             // 
-            this.crdHrsBox.Location = new System.Drawing.Point(120, 94);
+            this.crdHrsBox.Location = new System.Drawing.Point(134, 105);
             this.crdHrsBox.Name = "crdHrsBox";
             this.crdHrsBox.Size = new System.Drawing.Size(36, 20);
             this.crdHrsBox.TabIndex = 19;
             // 
-            // textBox8
+            // progIDDrpDwnBox
             // 
-            this.textBox8.Location = new System.Drawing.Point(244, 62);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(55, 20);
-            this.textBox8.TabIndex = 15;
+            this.progIDDrpDwnBox.FormattingEnabled = true;
+            this.progIDDrpDwnBox.Location = new System.Drawing.Point(137, 8);
+            this.progIDDrpDwnBox.Name = "progIDDrpDwnBox";
+            this.progIDDrpDwnBox.Size = new System.Drawing.Size(193, 21);
+            this.progIDDrpDwnBox.TabIndex = 20;
+            this.progIDDrpDwnBox.SelectedIndexChanged += new System.EventHandler(this.progIDDrpDwnBox_SelectedIndexChanged);
             // 
-            // label12
+            // label7
             // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(125, 64);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(120, 16);
-            this.label12.TabIndex = 16;
-            this.label12.Text = "Computer Science:";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(126, 94);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(143, 16);
-            this.label13.TabIndex = 17;
-            this.label13.Text = "Computer Engineering:";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(270, 94);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(55, 20);
-            this.textBox9.TabIndex = 18;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 20);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Program Name:";
             // 
             // Courses
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(668, 650);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progIDDrpDwnBox);
             this.Controls.Add(this.crdHrsBox);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.groupBox1);
@@ -338,5 +361,7 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ComboBox progIDDrpDwnBox;
+        private System.Windows.Forms.Label label7;
     }
 }
